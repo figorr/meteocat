fcbcd2462d4a3271f291060ce35410a537d6e360
figorr <77553975+figorr@users.noreply.github.com>
2024-11-15 20:34:37 +0100

    Add __init__.py file

--- meteocat/__init__.py
+++ meteocat/__init__.py
@@ -5,3 +5,15 @@ SPDX-License-Identifier: Apache-2.0
 For more details about this api, please refer to the documentation at
 https://gitlab.com/figorr1/meteocat
 """
+
+import logging
+
+# Configuración básica para el sistema de logging
+logging.basicConfig(
+    level=logging.INFO,  # Cambia a logging.DEBUG durante el desarrollo
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+_LOGGER.info("Paquete meteocat inicializado")
