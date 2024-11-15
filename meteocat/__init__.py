1e16459347501f88850784ea738c57fe7684e12b
figorr <77553975+figorr@users.noreply.github.com>
2024-11-15 20:05:02 +0100

    Add __init__.py file

---
+++ meteocat/__init__.py
@@ -0,0 +1,7 @@
+"""Python Package: Meteocat package to access weather data through the Meteocat API.
+
+SPDX-License-Identifier: Apache-2.0
+
+For more details about this api, please refer to the documentation at
+https://gitlab.com/figorr1/meteocat
+"""
