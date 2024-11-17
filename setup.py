67699de3aa561ccc0892bb83810628a1c97a65f8
figorr <77553975+figorr@users.noreply.github.com>
2024-11-17 15:23:16 +0100

    fix: setup.py version

--- setup.py
+++ setup.py
@@ -1,1 +1,10 @@
 from setuptools import setup
+
+# Leer la versión desde el archivo version.py
+with open("meteocat/version.py") as f:
+    exec(f.read())  # Ejecuta el archivo version.py para cargar la variable __version__
+
+setup(
+    name="meteocat",
+    version=__version__,  # Usa la versión cargada dinámicamente
+)
