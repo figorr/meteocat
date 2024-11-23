ddd8a072748fe5e06510af06ad7b9f74a7eb2b32
figorr <77553975+figorr@users.noreply.github.com>
2024-11-23 23:59:47 +0100

    fix: const.py

--- meteocat/const.py
+++ meteocat/const.py
@@ -1,3 +1,5 @@
 DOMAIN = "meteocat"
 BASE_URL = "https://api.meteo.cat"
 MUNICIPIS_LIST_URL = "/referencia/v1/municipis"
+MUNICIPIS_HORA_URL = "/pronostic/v1/municipalHoraria/{codi}"
+MUNICIPIS_DIA_URL = "/pronostic/v1/municipal/{codi}"
