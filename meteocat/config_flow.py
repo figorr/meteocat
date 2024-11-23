8016d2bc3be349846ed97f134dd0ad33a3cee81f
figorr <77553975+figorr@users.noreply.github.com>
2024-11-23 23:56:45 +0100

    fix config setup

--- meteocat/config_flow.py
+++ meteocat/config_flow.py
@@ -1,151 +1,126 @@
+from __future__ import annotations
+
 import logging
-import aiohttp
-import aiofiles
-import os
-import json
+from typing import Any
+
 import voluptuous as vol
-from homeassistant import config_entries
-from homeassistant.exceptions import ConfigEntryNotReady
-from .const import (
-    DOMAIN,
-    BASE_URL,
-    MUNICIPIS_LIST_URL
-)
+from aiohttp import ClientError
+
+from homeassistant.config_entries import ConfigEntry, ConfigFlow, ConfigFlowResult
+from homeassistant.const import CONF_API_KEY, CONF_NAME
+from homeassistant.core import callback
+from homeassistant.helpers import aiohttp_client, config_validation as cv
+
+from .const import DEFAULT_NAME, DOMAIN
+from .client import MeteocatClient
 
 _LOGGER = logging.getLogger(__name__)
 
-class MeteocatConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
-    """Manejo del flujo de configuración para Meteocat."""
+class MeteocatConfigFlow(ConfigFlow, domain=DOMAIN):
+    """Flujo de configuración para Meteocat."""
 
     VERSION = 1
 
     def __init__(self):
-        self.api_key = None
-        self.municipis_list = []
-        self.selected_municipi = None
-        self.path_municipis = MUNICIPIS_LIST_URL
+        self.api_key: str | None = None
+        self.municipis: list[dict[str, Any]] = []
+        self.selected_municipi: dict[str, Any] | None = None
 
-    async def async_step_user(self, user_input=None):
-        """Primer paso: Ingreso y validación de la API Key."""
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Primer paso: Solicitar la API Key."""
         errors = {}
 
         if user_input is not None:
-            if "cancel" in user_input:
-                return self.async_abort(reason="user_cancelled")
-            
-            self.api_key = user_input["api_key"]
-            status_code, municipios_data = await self._validate_and_download_municipis(self.api_key)
+            self.api_key = user_input[CONF_API_KEY]
 
-            if status_code == 200:
-                self.municipis_list = municipios_data
-                await self._save_municipis(municipios_data)
-                return await self.async_step_select_municipi()
-            else:
-                errors["base"] = self._map_error_code_to_user_message(status_code)
-
-        return self.async_show_form(
-            step_id="user",
-            data_schema=vol.Schema({
-                vol.Required("api_key"): str
-            }),
-            errors=errors,
-        )
-
-    async def async_step_select_municipi(self, user_input=None):
-        """Segundo paso: Selección de un municipio."""
-        errors = {}
-
-        if user_input is not None:
-            if "cancel" in user_input:
-                return self.async_abort(reason="user_cancelled")
-
-            selected_municipi = user_input.get("municipi")
-            matching_municipis = [
-                m for m in self.municipis_list if m["nom"].lower() == selected_municipi.lower()
-            ]
-
-            if matching_municipis:
-                self.selected_municipi = matching_municipis[0]
-                return await self.async_step_final()
-            else:
-                errors["municipi"] = "invalid_municipi"
-
-        municipis_names = [m["nom"] for m in self.municipis_list]
-        return self.async_show_form(
-            step_id="select_municipi",
-            data_schema=vol.Schema({
-                vol.Required("municipi"): vol.In(municipis_names)
-            }),
-            errors=errors,
-        )
-
-    async def async_step_final(self, user_input=None):
-        """Paso final: Confirmación de la integración."""
-        if user_input is not None:
-            # Crea la entrada de configuración
-            return self.async_create_entry(
-                title=f"Meteocat - {self.selected_municipi['nom']}",
-                data={
-                    "api_key": self.api_key,  # Guardamos la API Key
-                    "municipi": self.selected_municipi["nom"],  # Guardamos el nombre del municipio
-                    "codi": self.selected_municipi["codi"],  # Guardamos el código del municipio
-                }
+            client = MeteocatClient(
+                aiohttp_client.async_get_clientsession(self.hass), self.api_key
             )
 
-        # Muestra el formulario para confirmar la configuración
-        return self.async_show_form(
-            step_id="final",
-            data_schema=vol.Schema({}),
-            errors={},
+            try:
+                # Valida la API Key obteniendo la lista de municipios
+                self.municipis = await client.get_municipis()
+            except ClientError:
+                _LOGGER.error("Error al conectar con la API de Meteocat")
+                errors["base"] = "cannot_connect"
+            except Exception as ex:
+                _LOGGER.error("Error inesperado: %s", ex)
+                errors["base"] = "unknown"
+
+            if not errors:
+                # Si no hay errores, pasa al siguiente paso
+                return await self.async_step_select_municipi()
+
+        schema = vol.Schema({vol.Required(CONF_API_KEY): str})
+
+        return self.async_show_form(step_id="user", data_schema=schema, errors=errors)
+
+    async def async_step_select_municipi(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Segundo paso: Seleccionar el municipio."""
+        errors = {}
+
+        if user_input is not None:
+            # Encuentra el municipio seleccionado en la lista
+            selected_codi = user_input["municipi"]
+            self.selected_municipi = next(
+                (m for m in self.municipis if m["codi"] == selected_codi), None
+            )
+
+            if self.selected_municipi:
+                # Crea la entrada con la API Key y el municipio seleccionado
+                return self.async_create_entry(
+                    title=self.selected_municipi["nom"],
+                    data={
+                        CONF_API_KEY: self.api_key,
+                        "municipi": self.selected_municipi["nom"],
+                        "codi": self.selected_municipi["codi"],
+                    },
+                )
+            else:
+                errors["base"] = "municipi_not_found"
+
+        # Crear un esquema dinámico con los municipios disponibles
+        schema = vol.Schema(
+            {
+                vol.Required("municipi"): vol.In(
+                    {m["codi"]: m["nom"] for m in self.municipis}
+                )
+            }
         )
 
-    async def _validate_and_download_municipis(self, api_key):
-        """Valida la API Key y descarga la lista de municipios."""
-        url = f"{BASE_URL}{self.path_municipis}"
-        headers = {"X-Api-Key": api_key}
+        return self.async_show_form(
+            step_id="select_municipi", data_schema=schema, errors=errors
+        )
 
-        try:
-            async with aiohttp.ClientSession() as session:
-                async with session.get(url, headers=headers) as response:
-                    status_code = response.status
-                    if status_code == 200:
-                        municipios_data = await response.json()
-                        return status_code, municipios_data
+    @staticmethod
+    @callback
+    def async_get_options_flow(
+        config_entry: ConfigEntry,
+    ) -> ConfigFlowResult:
+        """Devuelve el flujo de opciones para esta configuración."""
+        return MeteocatOptionsFlowHandler(config_entry)
 
-                    error_details = await response.text()
-                    _LOGGER.warning(f"Error {status_code}: {error_details}")
-                    return status_code, None
 
-        except aiohttp.ClientError as e:
-            _LOGGER.critical(f"Error en la conexión: {e}")
-            return -1, None
+class MeteocatOptionsFlowHandler(ConfigFlow):
+    """Flujo de opciones para Meteocat."""
 
-    async def _save_municipis(self, municipios_data):
-        """Guarda la lista de municipios en un archivo JSON."""
-        carpeta_files = self.hass.config.path("custom_components", DOMAIN, "files")
-        archivo_salida = os.path.join(carpeta_files, "municipis_list.json")
+    def __init__(self, config_entry: ConfigEntry):
+        self.config_entry = config_entry
 
-        # Asegurarnos de que la carpeta exista
-        if not os.path.exists(carpeta_files):
-            os.makedirs(carpeta_files)
+    async def async_step_init(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Gestiona las opciones de configuración."""
+        return await self.async_step_user(user_input)
 
-        try:
-            # Convertimos la lista de municipios a una cadena JSON
-            municipios_json = json.dumps(municipios_data, ensure_ascii=False, indent=4)
-
-            # Guardar el archivo de manera asincrónica
-            async with aiofiles.open(archivo_salida, "w", encoding="utf-8") as file:
-                await file.write(municipios_json)
-            _LOGGER.info(f"Archivo guardado correctamente: {archivo_salida}")
-        except Exception as e:
-            _LOGGER.error(f"Error al guardar el archivo de municipios: {e}")
-
-    def _map_error_code_to_user_message(self, status_code):
-        """Mapea códigos de error HTTP a mensajes amigables para el usuario."""
-        return {
-            400: "bad_request",
-            403: "forbidden",
-            429: "rate_limit_exceeded",
-            500: "server_error",
-            -1: "connection_error",
-        }.get(status_code, "unknown_error")
+    async def async_step_user(
+        self, user_input: dict[str, Any] | None = None
+    ) -> ConfigFlowResult:
+        """Manejo de las opciones del usuario."""
+        return self.async_show_form(
+            step_id="user", data_schema=vol.Schema({}), errors={}
+        )
