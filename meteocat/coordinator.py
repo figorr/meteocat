e8d9678d14ddc625ca5cab3ea995d2b428bc9fe8
figorr <77553975+figorr@users.noreply.github.com>
2024-12-03 22:48:45 +0100

    fix: add two new classes for sensor and entity

--- meteocat/coordinator.py
+++ meteocat/coordinator.py
@@ -8,6 +8,7 @@ from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
 from homeassistant.exceptions import ConfigEntryNotReady
 
 from meteocatpy.data import MeteocatStationData
+from meteocatpy.forecast import MeteocatForecast
 from meteocatpy.exceptions import (
     BadRequestError,
     ForbiddenError,
@@ -16,18 +17,16 @@ from meteocatpy.exceptions import (
     UnknownAPIError,
 )
 
-from .const import (
-    DOMAIN,
-)
+from .const import DOMAIN
 
 _LOGGER = logging.getLogger(__name__)
 
 
-class MeteocatCoordinator(DataUpdateCoordinator):
-    """Coordinator para manejar la actualización de datos de la API de Meteocat."""
+class MeteocatSensorCoordinator(DataUpdateCoordinator):
+    """Coordinator para manejar la actualización de datos de los sensores."""
 
     def __init__(self, hass: HomeAssistant, api_key: str, station_id: str):
-        """Inicializa el coordinador de datos."""
+        """Inicializa el coordinador de datos para sensores."""
         self.api_key = api_key
         self.station_id = station_id
         self.meteocat_station_data = MeteocatStationData(api_key)
@@ -35,31 +34,70 @@ class MeteocatCoordinator(DataUpdateCoordinator):
         super().__init__(
             hass,
             _LOGGER,
-            name=f"{DOMAIN} Coordinator",
-            update_interval=timedelta(minutes=90),  # Intervalo de actualización de 90 minutos
+            name=f"{DOMAIN} Sensor Coordinator",
+            update_interval=timedelta(minutes=30),  # Intervalo de actualización de 30 minutos
         )
 
     async def _async_update_data(self):
         """
-        Actualiza los datos de la estación meteorológica desde la API de Meteocat.
+        Actualiza los datos de los sensores desde la API de Meteocat.
 
         Returns:
-            dict: Datos actualizados de la estación.
+            dict: Datos actualizados de los sensores.
         """
         try:
-            # Obtiene los datos de la estación organizados por variables
-            return await self.meteocat_station_data.get_station_data_with_variables(
-                self.station_id
-            )
+            return await self.meteocat_station_data.get_station_data_with_variables(self.station_id)
         except ForbiddenError as err:
-            _LOGGER.error("Acceso denegado: %s", err)
+            _LOGGER.error("Acceso denegado al obtener datos de sensores: %s", err)
             raise ConfigEntryNotReady from err
         except TooManyRequestsError as err:
-            _LOGGER.warning("Límite de solicitudes alcanzado: %s", err)
+            _LOGGER.warning("Límite de solicitudes alcanzado al obtener datos de sensores: %s", err)
             raise ConfigEntryNotReady from err
         except (BadRequestError, InternalServerError, UnknownAPIError) as err:
-            _LOGGER.error("Error al obtener datos de Meteocat: %s", err)
+            _LOGGER.error("Error al obtener datos de sensores: %s", err)
             raise
         except Exception as err:
-            _LOGGER.exception("Error inesperado: %s", err)
+            _LOGGER.exception("Error inesperado al obtener datos de sensores: %s", err)
+            raise
+
+
+class MeteocatEntityCoordinator(DataUpdateCoordinator):
+    """Coordinator para manejar la actualización de datos de las entidades de predicción."""
+
+    def __init__(self, hass: HomeAssistant, api_key: str, town_id: str):
+        """Inicializa el coordinador de datos para entidades de predicción."""
+        self.api_key = api_key
+        self.town_id = town_id
+        self.meteocat_forecast = MeteocatForecast(api_key)
+
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=f"{DOMAIN} Entity Coordinator",
+            update_interval=timedelta(hours=12),  # Intervalo de actualización de 3 horas
+        )
+
+    async def _async_update_data(self):
+        """
+        Actualiza los datos de las entidades de predicción desde la API de Meteocat.
+
+        Returns:
+            dict: Datos actualizados de predicción horaria y diaria.
+        """
+        try:
+            return {
+                "hourly_forecast": await self.meteocat_forecast.get_prediccion_horaria(self.town_id),
+                "daily_forecast": await self.meteocat_forecast.get_prediccion_diaria(self.town_id),
+            }
+        except ForbiddenError as err:
+            _LOGGER.error("Acceso denegado al obtener datos de predicción: %s", err)
+            raise ConfigEntryNotReady from err
+        except TooManyRequestsError as err:
+            _LOGGER.warning("Límite de solicitudes alcanzado al obtener datos de predicción: %s", err)
+            raise ConfigEntryNotReady from err
+        except (BadRequestError, InternalServerError, UnknownAPIError) as err:
+            _LOGGER.error("Error al obtener datos de predicción: %s", err)
+            raise
+        except Exception as err:
+            _LOGGER.exception("Error inesperado al obtener datos de predicción: %s", err)
             raise
