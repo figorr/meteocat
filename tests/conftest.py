87dbf9f42b729f76e42eb420e0b9e2c9ccad8481
figorr <77553975+figorr@users.noreply.github.com>
2024-12-06 11:49:14 +0100

    fix: new repo structure for HA

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,19 @@
 """Fixtures for testing."""
 
 import pytest
+import sys
+import os
+from homeassistant.setup import async_setup_component
+from homeassistant.core import HomeAssistant
 
+# Agregar el directorio raíz al PYTHONPATH
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
+# Habilitar la integración personalizada 'meteocat' en el entorno de pruebas
 @pytest.fixture(autouse=True)
-def auto_enable_custom_integrations(enable_custom_integrations):
+async def auto_enable_custom_integrations(hass: HomeAssistant):
     """Enable custom integrations."""
-    return
+    # Aquí se puede habilitar la integración personalizada 'meteocat'
+    await async_setup_component(hass, 'meteocat', {})
+    yield
+    # Si es necesario, puedes hacer limpieza después de cada prueba
