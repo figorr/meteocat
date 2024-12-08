d0e737302f9d7889a698468ba6ffb881d8da17c2
figorr <77553975+figorr@users.noreply.github.com>
2024-12-08 20:03:18 +0100

    fix: add solar global irradiance sensor

--- custom_components/meteocat/sensor.py
+++ custom_components/meteocat/sensor.py
@@ -18,6 +18,7 @@ from homeassistant.const import (
     UnitOfSpeed,
     UnitOfTemperature,
     UnitOfVolumetricFlux,
+    UnitOfIrradiance,
 )
 
 from .const import (
@@ -28,20 +29,29 @@ from .const import (
     HUMIDITY,
     PRESSURE,
     PRECIPITATION,
+    SOLAR_GLOBAL_IRRADIANCE,
     UV_INDEX,
     MAX_TEMPERATURE,
     MIN_TEMPERATURE,
     WIND_GUST,
-    VARIABLE_CODE_MAPPING,
+    WIND_SPEED_CODE,
+    WIND_DIRECTION_CODE,
+    TEMPERATURE_CODE,
+    HUMIDITY_CODE,
+    PRESSURE_CODE,
+    PRECIPITATION_CODE,
+    SOLAR_GLOBAL_IRRADIANCE_CODE,
+    UV_INDEX_CODE,
+    MAX_TEMPERATURE_CODE,
+    MIN_TEMPERATURE_CODE,
+    WIND_GUST_CODE,
 )
 
 from .coordinator import MeteocatSensorCoordinator
 
-
 @dataclass
 class MeteocatSensorEntityDescription(SensorEntityDescription):
     """A class that describes Meteocat sensor entities."""
-
 
 SENSOR_TYPES: tuple[MeteocatSensorEntityDescription, ...] = (
     MeteocatSensorEntityDescription(
@@ -50,7 +60,7 @@ SENSOR_TYPES: tuple[MeteocatSensorEntityDescription, ...] = (
         icon="mdi:weather-windy",
         device_class=SensorDeviceClass.WIND_SPEED,
         state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfSpeed.METERS_PER_SECOND,
+        native_unit_of_measurement=UnitOfSpeed.KILOMETERS_PER_HOUR,
     ),
     MeteocatSensorEntityDescription(
         key=WIND_DIRECTION,
@@ -91,6 +101,14 @@ SENSOR_TYPES: tuple[MeteocatSensorEntityDescription, ...] = (
         native_unit_of_measurement=UnitOfVolumetricFlux.MILLIMETERS_PER_HOUR,
     ),
     MeteocatSensorEntityDescription(
+        key=SOLAR_GLOBAL_IRRADIANCE,
+        name="Solar Global Irradiance",
+        icon="mdi:weather-sunny",
+        device_class=SensorDeviceClass.IRRADIANCE,
+        state_class=SensorStateClass.MEASUREMENT,
+        native_unit_of_measurement = UnitOfIrradiance.WATTS_PER_SQUARE_METER,
+    ),
+    MeteocatSensorEntityDescription(
         key=UV_INDEX,
         name="UV Index",
         icon="mdi:weather-sunny",
@@ -117,7 +135,7 @@ SENSOR_TYPES: tuple[MeteocatSensorEntityDescription, ...] = (
         icon="mdi:weather-windy",
         device_class=SensorDeviceClass.WIND_SPEED,
         state_class=SensorStateClass.MEASUREMENT,
-        native_unit_of_measurement=UnitOfSpeed.METERS_PER_SECOND,
+        native_unit_of_measurement=UnitOfSpeed.KILOMETERS_PER_HOUR,
     ),
 )
 
@@ -133,21 +151,30 @@ async def async_setup_entry(hass, entry, async_add_entities: AddEntitiesCallback
         for description in SENSOR_TYPES
     )
 
-
 class MeteocatSensor(CoordinatorEntity[MeteocatSensorCoordinator], SensorEntity):
     """Representation of a Meteocat sensor."""
+
+    CODE_MAPPING = {
+        WIND_SPEED: WIND_SPEED_CODE,
+        WIND_DIRECTION: WIND_DIRECTION_CODE,
+        TEMPERATURE: TEMPERATURE_CODE,
+        HUMIDITY: HUMIDITY_CODE,
+        PRESSURE: PRESSURE_CODE,
+        PRECIPITATION: PRECIPITATION_CODE,
+        SOLAR_GLOBAL_IRRADIANCE: SOLAR_GLOBAL_IRRADIANCE_CODE,
+        UV_INDEX: UV_INDEX_CODE,
+        MAX_TEMPERATURE: MAX_TEMPERATURE_CODE,
+        MIN_TEMPERATURE: MIN_TEMPERATURE_CODE,
+        WIND_GUST: WIND_GUST_CODE,
+    }
 
     def __init__(self, coordinator, description, entry_data):
         """Initialize the sensor."""
         super().__init__(coordinator)
         self.entity_description = description
-        self.api_key = entry_data["api_key"]  # Usamos la API key de la configuración
-        self._town_name = entry_data["town_name"]  # Usamos el nombre del municipio
-        self._town_id = entry_data["town_id"]  # Usamos el ID del municipio
-        self._variable_name = entry_data["variable_name"]  # Usamos el nombre de la variable
-        self._variable_id = entry_data["variable_id"]  # Usamos el ID de la variable
-        self._station_name = entry_data["station_name"]  # Usamos el nombre de la estación
-        self._station_id = entry_data["station_id"]  # Usamos el ID de la estación
+        self.api_key = entry_data["api_key"]
+        self._town_name = entry_data["town_name"]
+        self._town_id = entry_data["town_id"]
 
         # Unique ID for the entity
         self._attr_unique_id = f"{self._town_id}_{self.entity_description.key}"
@@ -155,26 +182,32 @@ class MeteocatSensor(CoordinatorEntity[MeteocatSensorCoordinator], SensorEntity)
     @property
     def native_value(self):
         """Return the state of the sensor."""
-        sensor_code = next(
-            (code for code, key in VARIABLE_CODE_MAPPING.items() if key == self.entity_description.key),
-            None,
-        )
-        
+        sensor_code = self.CODE_MAPPING.get(self.entity_description.key)
+
         if sensor_code is not None:
-            variable_data = next(
-                (var for var in self.coordinator.data.get("variables", []) if var["codi"] == sensor_code),
-                None,
-            )
-            if variable_data:
-                # Asume que quieres el último valor registrado
-                latest_reading = variable_data["lectures"][-1]
-                value = latest_reading.get("valor")
+            # Accedemos a las estaciones en el JSON recibido
+            stations = self.coordinator.data or []
+            for station in stations:
+                variables = station.get("variables", [])
 
-                # Convertir grados a direcciones cardinales para WIND_DIRECTION
-                if self.entity_description.key == WIND_DIRECTION and isinstance(value, (int, float)):
-                    return self._convert_degrees_to_cardinal(value)
+                # Filtramos por código
+                variable_data = next(
+                    (var for var in variables if var.get("codi") == sensor_code),
+                    None,
+                )
 
-                return value
+                if variable_data:
+                    # Obtenemos la última lectura
+                    lectures = variable_data.get("lectures", [])
+                    if lectures:
+                        latest_reading = lectures[-1]
+                        value = latest_reading.get("valor")
+
+                        # Convertimos grados a dirección cardinal para WIND_DIRECTION
+                        if self.entity_description.key == WIND_DIRECTION and isinstance(value, (int, float)):
+                            return self._convert_degrees_to_cardinal(value)
+
+                        return value
 
         return None
 
@@ -188,7 +221,6 @@ class MeteocatSensor(CoordinatorEntity[MeteocatSensorCoordinator], SensorEntity)
         index = round(degree / 22.5) % 16
         return directions[index]
 
-
     @property
     def device_info(self) -> DeviceInfo:
         """Return the device info."""
@@ -196,5 +228,5 @@ class MeteocatSensor(CoordinatorEntity[MeteocatSensorCoordinator], SensorEntity)
             identifiers={(DOMAIN, self._town_id)},
             name=self._town_name,
             manufacturer="Meteocat",
-            model="Meteocat API"
+            model="Meteocat API",
         )
