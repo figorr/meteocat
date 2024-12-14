461a423bd8596234bc182697d9f2725dd5234334
figorr <77553975+figorr@users.noreply.github.com>
2024-12-14 22:45:33 +0100

    fix: 0.1.33

--- custom_components/meteocat/__init__.py
+++ custom_components/meteocat/__init__.py
@@ -14,7 +14,7 @@ from .const import DOMAIN, PLATFORMS
 _LOGGER = logging.getLogger(__name__)
 
 # Versión
-__version__ = "0.1.32"
+__version__ = "0.1.33"
 
 def safe_remove(path: Path, is_folder: bool = False):
     """Elimina de forma segura un archivo o carpeta si existe."""
--- custom_components/meteocat/version.py
+++ custom_components/meteocat/version.py
@@ -1,2 +1,2 @@
 # version.py
-__version__ = "0.1.32"
+__version__ = "0.1.33"
