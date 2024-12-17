23bbf4465c79107e5326fc107b23cf4a0c71c70c
figorr <77553975+figorr@users.noreply.github.com>
2024-12-17 19:02:57 +0100

    fix: fix wind directions

--- custom_components/meteocat/sensor.py
+++ custom_components/meteocat/sensor.py
@@ -478,7 +478,7 @@ class MeteocatSensor(CoordinatorEntity[MeteocatSensorCoordinator], SensorEntity)
 
         directions = [
             "N", "NNE", "NE", "ENE", "E", "ESE", "SE", "SSE",
-            "S", "SSW", "SW", "WSW", "WO", "WNW", "NW", "NNW", "N",
+            "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW", "N",
         ]
         index = round(degree / 22.5) % 16
         return directions[index]
