cd8fb52c607e3a74308246136b6ac2d47b51e125
figorr <jdcuartero@yahoo.es>
2025-02-16 22:29:10 +0100

    fix: 2.2.5

--- custom_components/meteocat/__init__.py
+++ custom_components/meteocat/__init__.py
@@ -33,7 +33,7 @@ from .const import DOMAIN, PLATFORMS
 _LOGGER = logging.getLogger(__name__)
 
 # Versión
-__version__ = "2.2.4"
+__version__ = "2.2.5"
 
 # Definir el esquema de configuración CONFIG_SCHEMA
 CONFIG_SCHEMA = vol.Schema(
